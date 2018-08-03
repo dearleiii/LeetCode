3e57e36525102d15a7e55bbc0698325fbad507d8
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-03 16:32:03 -0400

    Create 6_ZighZag.java

---
+++ Strings_java/6_ZighZag.java
@@ -0,0 +1,22 @@
+import Java.lang
+
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        
+        // Combine the 3 lines, for simplicity, also decrease the corner case
+        String res = new String("");
+        
+        int gap = (numRows-2)*2;
+        for (int pos_row = 0; pos_row < numRows; pos_row++) { 
+            for (int pos = pos_row; pos < s.length(); pos+=(numRows*2-2)) {
+                res = res + s.charAt(pos);
+                if (pos_row != 0 && pos_row != numRows-1 && ((pos+gap) < s.length()) )
+                    res = res + s.charAt(pos+gap-2*pos_row);
+            }
+            // gap-=2;
+        }
+        
+        return res;
+    }
+}
