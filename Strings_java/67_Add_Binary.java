e0cc4b06fdacd12dac43be845f04cae7a069d953
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-06 17:37:44 -0400

    Create 67_Add_Binary.java

---
+++ Strings_java/67_Add_Binary.java
@@ -0,0 +1,7 @@
+class Solution {
+  public String addBinary(String a, String b) {
+  // Solution that no need to consider the length of a vs. b 
+  // while loop: until both pointer reaches the end 
+    
+  }
+}
