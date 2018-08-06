f28ba9ee7a7f77c3694034410b24ee266bb390a9
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-06 17:50:33 -0400

    Update 67_Add_Binary.java

--- Strings_java/67_Add_Binary.java
+++ Strings_java/67_Add_Binary.java
@@ -2,6 +2,30 @@ class Solution {
   public String addBinary(String a, String b) {
   // Solution that no need to consider the length of a vs. b 
   // while loop: until both pointer reaches the end 
-    
+    int len_a = a.length(), len_b = b.length();
+        int p_a = len_a-1, p_b = len_b-1;
+        StringBuffer res = new StringBuffer("");
+        int sum, carry=0;
+        
+        while (p_a >= 0 || p_b >=0) {
+            if (p_a <0) {
+                sum = (b.charAt(p_b)-'0' + carry) % 2;
+                carry = (b.charAt(p_b)-'0' + carry) /2;
+            }
+            else if (p_b <0) { // add the rest of a 
+                sum = (a.charAt(p_a) -'0'+ carry ) %2;
+                carry = (a.charAt(p_a) -'0' + carry) /2;
+            }
+            else {
+                // add both a & b part 
+                sum = (a.charAt(p_a)-'0' + b.charAt(p_b)-'0' + carry) % 2;
+                carry = (a.charAt(p_a)-'0' + b.charAt(p_b)-'0' + carry) / 2;
+            }
+            res.append(sum);
+            p_a--; p_b--;
+        }
+        if (carry == 1) 
+            res.append(carry);
+        return res.reverse().toString();
   }
 }
