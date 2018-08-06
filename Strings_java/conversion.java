d4863c2bb02e2e3c447d17579e7c0d4f0ea860c8
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-06 16:17:03 -0400

    Create conversion.java

---
+++ Strings_java/conversion.java
@@ -0,0 +1,9 @@
+// Int -> String conversion 
+Integer.toString(int);
+String.valueOf(int);
+
+// Char -> String conversion 
+Character.toString(char);
+String.valueOf(char);
+// shortcut: str concat 
+String s = ""+ 's';
