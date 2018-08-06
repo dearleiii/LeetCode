b0c45e0cb96aba56f0dc2defbcb6c665f8c0a9b5
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-06 14:32:11 -0400

    Create HashMap.java

---
+++ Hash/HashMap.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class IterateHashMap {
+  public static void main(String[] args) {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("key1", "value1");
+    map.put("key2", "value2");
+    
+    for (Map.Entry<String, String> entry: map.entrySet()) {
+      System.out.println(entry.getKey() + "=" + entry.getValue());
+    }
+  }
+}
