cd56d5ff30e07b896220008d6264ee0f6706f856
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-08 11:06:12 -0400

    Update 0808.java

--- dailyReport/0808.java
+++ dailyReport/0808.java
@@ -8,3 +8,9 @@ Character.toUpperCase(char1);
 
 // 3. Convert between String[] array, and List<String>
 str_array = str_list.toArray(new String[0]);
+
+// 4. char vs. Character
+// char is a primitive type that represents a single 16 bit Unicode character 
+// while Character is a wrapper class that allows us to use char primitive concept in OOP-kind of way.
+char ch = 'a';
+Character.toUpperCase(ch);
