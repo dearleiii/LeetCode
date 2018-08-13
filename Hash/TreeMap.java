979ffaed5fbd4729d2fc34edcc238c62365d848d
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-13 15:35:19 -0400

    Create TreeMap.java

---
+++ Hash/TreeMap.java
@@ -0,0 +1,16 @@
+// Prior: HashMap
+  // stores keys and API, for store and retrival at O(1)
+import java.util.HashMap;
+
+// char: 
+  // 1. does not maintain order of keys/ values 
+  // 2. get/put/containsKey(): O(1)
+  
+  
+  
+// TreeMap: maintain sorted order for keys 
+import java.util.TreeMap;
+
+// char: 
+  // red-black tree in background 
+  
