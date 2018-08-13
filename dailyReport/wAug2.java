9144c91a9d4b630df219ba86f6f11e83c55e7d8b
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-13 12:00:35 -0400

    Create wAug2.java

---
+++ dailyReport/wAug2.java
@@ -0,0 +1,14 @@
+// 1.Convert string to int 
+String number = "10";
+int result = Integer.parseInt(number);
+System.out.println(result);
+
+// m2:
+String number = "10";
+Integer result = Integer.valueOf(number);		
+System.out.println(result);
+
+// 2. Tell character is a letter/ number 
+Character.isDigit(string.charAt(index)) 
+Character.isLetter(string.charAt(index))
+
