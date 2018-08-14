80fce7b2adc8743e292a0b4488c47135b056cded
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-14 10:21:15 -0400

    Create cheatSheet.java

---
+++ cheatSheet.java
@@ -0,0 +1,11 @@
+// 1. HashMap 
+containsKey(Object key)
+containsValue(Object value)
+get(Object key). // Returns or null if this map contains no mapping for the key.
+map.getOrDefault(idNum, "John Doe"));
+
+// 2. HashSet
+
+// 2. Conversion 
+String str = "testString";
+char[] charArray = str.toCharArray();
