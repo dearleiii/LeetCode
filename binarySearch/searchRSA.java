73569de0c1919f6017fa2423ad068f2623eb5443
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-15 17:00:08 -0400

    Update searchRSA.java

--- binarySearch/searchRSA.java
+++ binarySearch/searchRSA.java
@@ -27,4 +27,42 @@ public class Solution {
 }
 
 // Method2. B.S 1 time 
+    // 1-time search, visualize to discussion different possibilities 
+    // discuss mid-line firse 
+        // discuss within each situation
+public class Solution {
+    int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return -1;
+        
+        int start = 0; 
+        int end = nums.length-1;
+        int mid;
+        
+        while (start+1 < end) {
+            mid = start+ (end - start) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[start] < nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid;
+                } else {
+                    start = mid;
+                }    
+            } else { 
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid;
+                } else {
+                    end = mid;
+                }
+            }
+        }
+        
+        if (nums[start] == target) return start;
+        if (nums[end] == target) return end;
+        return -1;
+    }
+}
 
+
+// !! Note!!!
+while (start+1 < end) 
+    // to avoid time limit exceed
