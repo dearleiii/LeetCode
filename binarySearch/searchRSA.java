9637779ee7786d351c645d8f85b344ebc09e93d3
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-15 16:21:37 -0400

    Create searchRSA.java

---
+++ binarySearch/searchRSA.java
@@ -0,0 +1,30 @@
+// Method1. B.S 2 times 
+
+public class Solution {
+    int search(int[] nums, int target) {
+        int lo=0,hi=nums.length-1;
+        // find the index of the smallest value using binary search.
+        // Loop will terminate since mid < hi, and lo or hi will shrink by at least 1.
+        // Proof by contradiction that mid < hi: if mid==hi, then lo==hi and loop would have been terminated.
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(nums[mid]>nums[hi]) lo=mid+1;
+            else hi=mid;
+        }
+        // lo==hi is the index of the smallest value and also the number of places rotated.
+        int rot=lo;
+        lo=rot;hi=rot-1;
+        // The usual binary search and accounting for rotation.
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            int realmid=(mid+rot)%nums.length;
+            if(nums[realmid]==target)return realmid;
+            if(nums[realmid]<target)lo=mid+1;
+            else hi=mid-1;
+        }
+        return -1;
+    }
+}
+
+// Method2. B.S 1 time 
+
