81d2f8f176c200c70de862be8bf49cbaffd5bce7
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-16 11:30:10 -0400

    Create findDuplicates.java

---
+++ array/findDuplicates.java
@@ -0,0 +1,20 @@
+// can solve in 1 for loop, time complexity diff. in O(n) vs. O(2n)
+
+// For questions mentioning: array of integers, 1 ≤ a[i] ≤ n
+  // -> can be solved by index array  
+
+public class Solution {
+    // when find a number i, flip the number at position i-1 to negative. 
+    // if the number at position i-1 is already negative, i is the number that occurs twice.
+    
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
