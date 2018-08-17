f8392e763b357a0b15e54a7fc1444d6ef1ad0f59
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-16 23:26:12 -0400

    Create isPalindrome.java

---
+++ twoPointers/isPalindrome.java
@@ -0,0 +1,31 @@
+// consider coner cases: ".,"; "0P"; 
+// need to consider digits as well
+
+// function: Character.isLetterOrDigit
+
+public class Solution {
+    public boolean isPalindrome(String s) {
+        if (s.isEmpty()) {
+        	return true;
+        }
+        int head = 0, tail = s.length() - 1;
+        char cHead, cTail;
+        while(head <= tail) {
+        	cHead = s.charAt(head);
+        	cTail = s.charAt(tail);
+        	if (!Character.isLetterOrDigit(cHead)) {
+        		head++;
+        	} else if(!Character.isLetterOrDigit(cTail)) {
+        		tail--;
+        	} else {
+        		if (Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
+        			return false;
+        		}
+        		head++;
+        		tail--;
+        	}
+        }
+        
+        return true;
+    }
+}
