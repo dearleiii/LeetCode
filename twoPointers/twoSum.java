3713ebe6ed9ceb55985a921f8dd3bf0a4568c919
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-17 15:33:23 -0400

    Update twoSum.java

--- twoPointers/twoSum.java
+++ twoPointers/twoSum.java
@@ -19,3 +19,17 @@ class Solution {
 }
 
 // O(1) method: 2 pointers front && end
+    // since it's sorted, can just move one for the unique answer!
+    public int[] twoSum(int[] numbers, int target) {
+        int front = 0, end = numbers.length-1;
+        int[] output = new int[2];
+        while (front < end) {
+            if ((numbers[front] + numbers[end]) == target) {
+                output[0] = front+1; output[1] = end+1; return output;
+            } else if ((numbers[front] + numbers[end]) > target) {
+                end--;
+            } else front++;
+        }
+        
+        return output;
+    }
