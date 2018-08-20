d9d8b052fd67cfe9a4b72df2441164a7def60011
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-08-19 20:05:34 -0400

    Update employeeImportance.java

--- bfs/employeeImportance.java
+++ bfs/employeeImportance.java
@@ -1,5 +1,29 @@
 // Before solving the question, need to ask & make clear: what is importance score, does it include indirect sub-es? 
 
+// My code: runtime error for [[2, 5, []]] 
+class Solution {
+    Map<Integer, Employee> emap;
+    public int getImportance(List<Employee> employees, int queryid) {
+        emap = new HashMap<>();
+        int i = 1;
+        for (Employee employee: employees) {
+            emap.put(i++, employee);
+        }
+        return dfs(queryid);
+    }
+    
+    private int dfs(int queryid) {
+        Employee person = emap.get(queryid);
+        int totImp = person.importance;
+        for (int subord: person.subordinates) {
+            totImp += dfs(subord);
+        } 
+        return totImp;
+    }
+
+}
+
+// Standard solution 
 class Solution {
     // define emap as global variable, since called in both functions 
     Map<Integer, Employee> emap;
