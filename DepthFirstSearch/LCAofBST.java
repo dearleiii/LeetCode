1f88106bbf8e1595beaefcb44606a178812de7f8
RachelChen <33907535+dearleiii@users.noreply.github.com>
2018-09-09 20:39:56 -0400

    Create LCAofBST.java

---
+++ DepthFirstSearch/LCAofBST.java
@@ -0,0 +1,14 @@
+// V.S LCA of binary tree
+  // Note!! can use BST special: root.val > root.left.val && root.val < root.right.val
+
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
+        }
+    }
+}
