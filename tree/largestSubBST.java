4cafabc2a72d61c1c9485976f0c78b10bc134e44
RachelChen <lei.chen2@duke.edu>
2018-09-11 13:44:17 -0400

    Update largestSubBST.java

--- tree/largestSubBST.java
+++ tree/largestSubBST.java
@@ -24,4 +24,43 @@ class Solution {
 }
 
 // Official:
+    // Since need to keep track of more values: isBST, node number, smallest of subtree, largest of subtree 
+    // -> create SuperNode!
 
+class Solution {
+    public class SuperNode {
+        int ans; 
+        int small, large;
+        boolean isBST;
+        public SuperNode() {
+            ans = 0;
+            isBST = true;
+            small = Integer.MAX_VALUE;
+            large = -Integer.MAX_VALUE;
+        }
+    }
+    
+    public int largestBSTSubtree(TreeNode root) {
+        return dfs(root).ans;
+    }
+    
+    public SuperNode dfs(TreeNode node) {
+        if (node == null) return new SuperNode();
+        SuperNode now = new SuperNode();
+        SuperNode left = dfs(node.left);
+        SuperNode right = dfs(node.right);
+        
+        if (left.small < node.val) now.small = left.small;
+        else now.small = node.val;  // not a BST
+        
+        now.large = Math.max(right.large, node.val);
+        if (left.isBST && right.isBST && left.large < node.val && right.small >= node.val) {
+            now.ans = left.ans + right.ans + 1;
+            now.isBST = true;
+        } else {
+            now.ans = Math.max(left.ans, right.ans);
+            now.isBST = false;
+        }
+        return now;
+    }
+}
