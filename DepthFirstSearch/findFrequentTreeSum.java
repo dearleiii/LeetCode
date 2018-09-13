7a6800fc33ecb00425ae46d26715df5066b3aca8
RachelChen <lei.chen2@duke.edu>
2018-09-13 17:21:11 -0400

    Update findFrequentTreeSum.java

--- DepthFirstSearch/findFrequentTreeSum.java
+++ DepthFirstSearch/findFrequentTreeSum.java
@@ -1,27 +1,39 @@
- private Map<Integer, Integer> map = new HashMap<>();
-    private int maxFreq = 0;
-    public int[] findFrequentTreeSum(TreeNode root) {
-        int sum = findTreeSum(root);
-        
-        List<Integer> result = new ArrayList<>();
-        for (Integer key: map.keySet()) {
-            if (map.get(key) == maxFreq) result.add(key);
-        }
+// Improve performance from 69% -> 76% by combine all helper, no discussing cases 
 
-        int[] result_arr = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) 
-            result_arr[i] = result.get(i);
-        return result_arr;
+class Solution {
+    Map<Integer, Integer> freq = new HashMap<>();
+    int maxFreq = Integer.MIN_VALUE;
+    
+    public int[] findFrequentTreeSum(TreeNode root) {
+        int tot = helper(root);
+        List<Integer> maxSum = new ArrayList<>();
+        
+        for (int sum: freq.keySet()) {
+            if (freq.get(sum) == maxFreq) 
+                maxSum.add(sum);
+        }
+        int[] sumArr = new int[maxSum.size()];
+        for (int i = 0; i < sumArr.length; i++) 
+            sumArr[i] = maxSum.get(i);
+        return sumArr;
     }
     
-    private int findTreeSum(TreeNode root) {
+    private int helper(TreeNode root) {
         if (root == null) return 0;
+        /*
+        if (root.left == null && root.right == null) {
+            freq.put(root.val, freq.getOrDefault(root.val, 0) + 1);
+            if (freq.get(root.val) > maxFreq) 
+                maxFreq = freq.get(root.val);
+            return root.val;
+        }*/
         
-        int left = findTreeSum(root.left);
-        int right = findTreeSum(root.right);
-        int currSum = left + right + root.val;
-        int count = map.getOrDefault(currSum, 0)+1;
-        map.put(currSum, count);
-        maxFreq = Math.max(maxFreq, count);
-        return currSum;
+        int leftSum = helper(root.left);
+        int rightSum = helper(root.right);
+        int curr = root.val + leftSum + rightSum;
+        freq.put(curr, freq.getOrDefault(curr, 0)+1);
+        if (freq.get(curr) > maxFreq) 
+            maxFreq = freq.get(curr);
+        return curr;
     }
+}
